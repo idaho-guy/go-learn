043bc4875dda19038eed019f488181464fe250ff
marksage <sage.mark.alan@gmail.com>
2025-08-12 07:08:34 -0500

    pointers before

---
+++ go-basics/pointers/pointers.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 32 // regular variable
+	fmt.Println("Age:", age)
+	adultYears := getAdultYears((age))
+	fmt.Println(adultYears)
+}
+func getAdultYears(age int) int {
+	return age - 18
+}
