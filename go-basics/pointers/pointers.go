17bd2f873a17eea44803aa0070b7d3dd4e982c9c
marksage <sage.mark.alan@gmail.com>
2025-08-12 07:22:11 -0500

    write over value in memory

--- go-basics/pointers/pointers.go
+++ go-basics/pointers/pointers.go
@@ -7,9 +7,9 @@ func main() {
 
 	agePointer := &age               // address of age, 0xc000090020
 	fmt.Println("Age:", *agePointer) // * to de-reference pointer, display 32
-	adultYears := getAdultYears(agePointer)
-	fmt.Println(adultYears)
+	updateAgeToAdultYears(agePointer)
+	fmt.Println(age)
 }
-func getAdultYears(age *int) int { // copy of age will not be sent to function
-	return *age - 18
+func updateAgeToAdultYears(age *int) { // copy of age will not be sent to function
+	*age = *age - 18 // override in memory
 }
