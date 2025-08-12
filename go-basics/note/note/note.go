57a41ccfc752cc82badbc1f8e58a6f5e2d21aeef
marksage <sage.mark.alan@gmail.com>
2025-08-12 16:45:44 -0500

    note struct example

---
+++ go-basics/note/note/note.go
@@ -0,0 +1,28 @@
+package note
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type Note struct {
+	title     string
+	content   string
+	createdAt time.Time
+}
+
+func (note Note) Display() {
+	fmt.Printf("Your note titled %v has the folling content\n\n%v", note.title, note.content)
+}
+
+func New(title, content string) (Note, error) {
+	if title == "" || content == "" {
+		return Note{}, errors.New("Invalid input")
+	}
+	return Note{
+		title:     title,
+		content:   content,
+		createdAt: time.Now(),
+	}, nil
+}
