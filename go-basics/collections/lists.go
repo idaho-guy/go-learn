f0eff900e299180a1d148695748d120228afc359
marksage <sage.mark.alan@gmail.com>
2025-08-13 16:22:34 -0500

    slice examples

--- go-basics/collections/lists.go
+++ go-basics/collections/lists.go
@@ -10,4 +10,7 @@ func main() {
 	prices := [4]float64{10.0, 9.9, 20.0, 12.3}
 	fmt.Println(prices)
 	fmt.Println(productNames)
+	//featuredPrices := prices[1:3] // 9.9 20
+	var featuredPrices = prices[:3] // 10 9.9 20
+	fmt.Println(featuredPrices)
 }
