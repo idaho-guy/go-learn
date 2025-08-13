a2547ae85a3fcde753b1a91f07a2eeb2f9370e6f
marksage <sage.mark.alan@gmail.com>
2025-08-13 16:17:51 -0500

    initialize array with no elements populated

--- go-basics/collections/lists.go
+++ go-basics/collections/lists.go
@@ -5,6 +5,9 @@ import (
 )
 
 func main() {
+	var productNames [4]string
+	productNames[0] = "Dishwashing Liquid"
 	prices := [4]float64{10.0, 9.9, 20.0, 12.3}
-	fmt.Println(prices[0])
+	fmt.Println(prices)
+	fmt.Println(productNames)
 }
