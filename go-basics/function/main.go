84c4ebf83ce84d5952152a1ad2f2293cf760ec20
marksage <sage.mark.alan@gmail.com>
2025-08-14 18:53:12 -0500

    use type (for when function signatures get long

--- go-basics/function/main.go
+++ go-basics/function/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+type transformFn func(int) int
+
 func main() {
 	numbers := []int{1, 2, 3, 5}
 	doubled := transFormNumbers(&numbers, double)
@@ -10,7 +12,7 @@ func main() {
 	fmt.Println(tripled)
 }
 
-func transFormNumbers(numbers *[]int, transform func(int) int) []int {
+func transFormNumbers(numbers *[]int, transform transformFn) []int {
 	dNumbers := []int{}
 	for _, val := range *numbers {
 		dNumbers = append(dNumbers, transform(val))
