c16ad2e5aaa5681b201124342e70b0b81b698aca
marksage <sage.mark.alan@gmail.com>
2025-08-14 08:29:06 -0500

    using make with map

--- go-basics/collections/main.go
+++ go-basics/collections/main.go
@@ -8,5 +8,10 @@ func main() {
 	userNames[1] = "Baby"
 	// userNames[2] = "Fail" // fails because initial size of 2 is allocated
 	userNames = append(userNames, "Max", "Mark", "Clara", "Tommy", "Emma")
+	// when adding Tommy and Emma, all new array is created
 	fmt.Println(userNames)
+
+	courseRatings := make(map[string]float64, 3) // pre-allocate map for 3 elements
+	courseRatings["go"] = 4.6
+	courseRatings["react"] = 4.7
 }
