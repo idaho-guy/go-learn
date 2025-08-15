60575f17f6735cc49630bc2aa4c07d1d381dfd80
marksage <sage.mark.alan@gmail.com>
2025-08-14 19:13:58 -0500

    demonstrate a closure

--- go-basics/function/main.go
+++ go-basics/function/main.go
@@ -4,10 +4,15 @@ import "fmt"
 
 func main() {
 	numbers := []int{1, 2, 3}
+	double := createTransformer(2)
+	triple := createTransformer(3)
 
 	transformed := transformNumbers(&numbers, func(number int) int {
-		return number * 2
+		return number * 5
 	})
+
+	fmt.Println(transformNumbers(&numbers, double))
+	fmt.Println(transformNumbers(&numbers, triple))
 
 	fmt.Println(transformed)
 }
@@ -20,4 +25,13 @@ func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	}
 
 	return dNumbers
+}
+
+func createTransformer(factor int) func(int) int {
+	// using factor from funtion's outer scope, below is a closure
+	// factor will be availble in double and triple always (locked in)
+	return func(number int) int {
+		return number * factor
+	}
+
 }
