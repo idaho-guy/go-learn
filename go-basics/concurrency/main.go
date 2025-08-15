f4ebdebb04363779c8617ea2f9666ce20e990169
marksage <sage.mark.alan@gmail.com>
2025-08-15 15:00:15 -0500

    demostrate closing a chan

--- go-basics/concurrency/main.go
+++ go-basics/concurrency/main.go
@@ -13,6 +13,7 @@ func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
 	doneChan <- true
+	close(doneChan) // only works if you know which operation takes the longes
 }
 
 func main() {
@@ -23,5 +24,8 @@ func main() {
 	go slowGreet("How ... are ... you ...?", done)
 
 	go greet("I hope you're liking the course!", done)
-	<-done //will contine after done is true
+	for doneChan := range done {
+		fmt.Println(doneChan)
+	}
+	// <-done //will contine after done us true
 }
