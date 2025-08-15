c8fb52c2e924a368a624bae38871c422b3e24182
marksage <sage.mark.alan@gmail.com>
2025-08-14 19:01:54 -0500

    move file

---
+++ go-basics/function/functionsarevalues/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type transformFn func(int) int
+
+func main() {
+	numbers := []int{1, 2, 3, 5}
+	doubled := transFormNumbers(&numbers, getTransformerFunction())
+	fmt.Println(doubled)
+	tripled := transFormNumbers(&numbers, triple)
+	fmt.Println(tripled)
+}
+
+func transFormNumbers(numbers *[]int, transform transformFn) []int {
+	dNumbers := []int{}
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, transform(val))
+	}
+	return dNumbers
+}
+
+func getTransformerFunction() transformFn {
+	return double
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
+}
